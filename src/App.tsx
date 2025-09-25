23f938857c94216c8612042a8c47e5ffe2d05598
Vivek <vivekpradhan736@gmail.com>
2025-09-25 15:15:27 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import Dashboard from "./pages/Dashboard";
 import Maintenance from "./pages/Maintenance";
 import ResaleValue from "./pages/ResaleValue";
